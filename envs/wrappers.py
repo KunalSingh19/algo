70374ed149584b3469eeec3201c07f31001846fa
KunalSingh19 <73868343+KunalSingh19@users.noreply.github.com>
2025-06-29 10:39:18 +0530

    Update wrappers.py

--- envs/wrappers.py
+++ envs/wrappers.py
@@ -1,1 +1,11 @@
-# wrappers.py 
+# Example: Observation normalization wrapper
+import gymnasium as gym
+
+class ObsNormWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.obs_mean = 0
+        self.obs_std = 1
+
+    def observation(self, obs):
+        return (obs - self.obs_mean) / (self.obs_std + 1e-8)
