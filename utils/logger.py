316f921b1c6cc3e2e2f3ee5beb38d51ab0cdd58d
KunalSingh19 <73868343+KunalSingh19@users.noreply.github.com>
2025-06-29 10:42:52 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,15 @@
-# logger.py 
+import logging
+
+def setup_logger(name="RLLogger", log_file=None, level=logging.INFO):
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    if log_file:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    return logger
+  
