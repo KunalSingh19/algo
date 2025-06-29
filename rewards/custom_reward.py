4adffc62e01b885814df2d007e2a0099f75b16ff
KunalSingh19 <73868343+KunalSingh19@users.noreply.github.com>
2025-06-29 10:42:18 +0530

    Update custom_reward.py

--- rewards/custom_reward.py
+++ rewards/custom_reward.py
@@ -1,1 +1,14 @@
-# custom_reward.py 
+from .reward_base import RewardBase
+from .sharpe_reward import SharpeReward
+from .drawdown_penalty import DrawdownPenalty
+
+class CustomReward(RewardBase):
+    def __init__(self, alpha=1.0, beta=1.0):
+        super().__init__()
+        self.sharpe = SharpeReward()
+        self.drawdown = DrawdownPenalty()
+        self.alpha = alpha
+        self.beta = beta
+
+    def compute(self, returns, portfolio_values):
+        return self.alpha * self.sharpe.compute(returns) + self.beta * self.drawdown.compute(portfolio_values)
